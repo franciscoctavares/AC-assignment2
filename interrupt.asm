ea548850ca01b9e33bde15ea60ec6c6391cb49bd
franciscoctavares <francisco.ctavares@protonmail.com>
2024-10-31 13:42:24 +0000

    usar macros para especificar os offsets

--- interrupt.asm
+++ interrupt.asm
@@ -1,8 +1,6 @@
-.include "macros.asm"
-
 .data
 
-PCB_BLOCKS: .space 1440 # 132 bytes x 10 Tasks
+PCB_BLOCKS: .space 1440 # 144 bytes x 10 Tasks
 PCB_STACKS: .space 1000 # (4 bytes x 25 variables) x 10 PCB's
 RUNNING: .word 0x00000000
 READY: .word 0x00000000
@@ -10,50 +8,12 @@ LAST_READY: .word 0x00000000
 
 RUNNING_str: .asciiz "RUNNING: "
 READY_str: .asciiz "READY: "
+next_str: .asciiz " -> "
 
 ALL_INT_MASK: .word 0x0000ff00
 KBD_INT_MASK: .word 0x00010000
 
 RCR: .word 0xffff0000
-
-next_str: .asciiz " -> "
-
-.eqv at 0
-.eqv v0 4
-.eqv v1 8
-.eqv a0 12
-.eqv a1 16
-.eqv a2 20
-.eqv a3 24
-.eqv t0 28
-.eqv t1 32
-.eqv t2 36
-.eqv t3 40
-.eqv t4 44
-.eqv t5 48
-.eqv t6 52
-.eqv t7 56
-.eqv s0 60
-.eqv s1 64
-.eqv s2 68
-.eqv s3 72
-.eqv s4 76
-.eqv s5 80
-.eqv s6 84
-.eqv s7 88
-.eqv t8 92
-.eqv t9 96
-.eqv k0 100
-.eqv k1 104
-.eqv gp 108
-.eqv sp 112
-.eqv fp 116
-.eqv ra 120
-.eqv hi 124
-.eqv lo 128
-.eqv epc 132
-.eqv PROCESS_ID 136
-.eqv NEXT_PCB 140
 
 .text
 int_enable:
@@ -141,51 +101,51 @@ save_running_task_registers:
 	
 	lw $t0, RUNNING
 	
-	sw $at, 0($t0)
-	sw $v0, 4($t0)
-	sw $v1, 8($t0)
-	sw $a0, 12($t0)
-	sw $a1, 16($t0)
-	sw $a2, 20($t0)
-	sw $a3, 24($t0)
-	#sw $t0, 28($t0)
-	sw $t1, 32($t0)
-	sw $t2, 36($t0)
-	sw $t3, 40($t0)
-	sw $t4, 44($t0)
-	sw $t5, 48($t0)
-	sw $t6, 52($t0)
-	sw $t7, 56($t0)
-	sw $s0, 60($t0)
-	sw $s1, 64($t0)
-	sw $s2, 68($t0)
-	sw $s3, 72($t0)
-	sw $s4, 76($t0)
-	sw $s5, 80($t0)
-	sw $s6, 84($t0)
-	sw $s7, 88($t0)
-	sw $t8, 92($t0)
-	sw $t9, 96($t0)
-	sw $k0, 100($t0)
-	sw $k1, 104($t0)
-	sw $gp, 108($t0)
-	sw $sp, 112($t0)
-	sw $fp, 116($t0)
-	sw $ra, 120($t0)
+	sw $at, at($t0)
+	sw $v0, v0($t0)
+	sw $v1, v1($t0)
+	sw $a0, a0($t0)
+	sw $a1, a1($t0)
+	sw $a2, a2($t0)
+	sw $a3, a3($t0)
+	#sw $t0, t0($t0)
+	sw $t1, t1($t0)
+	sw $t2, t2($t0)
+	sw $t3, t3($t0)
+	sw $t4, t4($t0)
+	sw $t5, t5($t0)
+	sw $t6, t6($t0)
+	sw $t7, t7($t0)
+	sw $s0, s0($t0)
+	sw $s1, s1($t0)
+	sw $s2, s2($t0)
+	sw $s3, s3($t0)
+	sw $s4, s4($t0)
+	sw $s5, s5($t0)
+	sw $s6, s6($t0)
+	sw $s7, s7($t0)
+	sw $t8, t8($t0)
+	sw $t9, t9($t0)
+	sw $k0, k0($t0)
+	sw $k1, k1($t0)
+	sw $gp, gp($t0)
+	sw $sp, sp($t0)
+	sw $fp, fp($t0)
+	sw $ra, ra($t0)
 
 	lw $t1, RUNNING
 
 	mfhi $t0
-	sw $t0, 124($t1) # hi
+	sw $t0, hi($t1) # hi
 	mflo $t0
-	sw $t0, 128($t1) # lo
+	sw $t0, lo($t1) # lo
 			
 	mfc0 $t0, $14
-	sw $t0, 132($t1) # epc
+	sw $t0, epc($t1) # epc
 	
 	lw $t0, 0($sp)
 	lw $t1, RUNNING
-	sw $t0, 28($t1)
+	sw $t0, t0($t1)
 	
 	addi $sp, $sp, 4
 	
@@ -194,41 +154,41 @@ save_running_task_registers:
 load_next_task_registers:
 	lw $t0, RUNNING
 
-	lw $at, 0($t0)
-	lw $v0, 4($t0)
-	lw $v1, 8($t0)
-	lw $a0, 12($t0)
-	lw $a1, 16($t0)
-	lw $a2, 20($t0)
-	lw $a3, 24($t0)
+	lw $at, at($t0)
+	lw $v0, v0($t0)
+	lw $v1, v1($t0)
+	lw $a0, a0($t0)
+	lw $a1, a1($t0)
+	lw $a2, a2($t0)
+	lw $a3, a3($t0)
 	
-	lw $t1, 132($t0)
+	lw $t1, epc($t0)
 	mtc0 $t1, $14 # load next task's epc
 	
-	lw $t1, 32($t0)
-	lw $t2, 36($t0)
-	lw $t3, 40($t0)
-	lw $t4, 44($t0)
-	lw $t5, 48($t0)
-	lw $t6, 52($t0)
-	lw $t7, 56($t0)
-	lw $s0, 60($t0)
-	lw $s1, 64($t0)
-	lw $s2, 68($t0)
-	lw $s3, 72($t0)
-	lw $s4, 76($t0)
-	lw $s5, 80($t0)
-	lw $s6, 84($t0)
-	lw $s7, 88($t0)
-	lw $t8, 92($t0)
-	lw $t9, 96($t0)
-	lw $k0, 100($t0)
-	lw $k1, 104($t0)
-	lw $gp, 108($t0)
-	lw $sp, 112($t0)
-	lw $fp, 116($t0)
+	lw $t1, t1($t0)
+	lw $t2, t2($t0)
+	lw $t3, t3($t0)
+	lw $t4, t4($t0)
+	lw $t5, t5($t0)
+	lw $t6, t6($t0)
+	lw $t7, t7($t0)
+	lw $s0, s0($t0)
+	lw $s1, s1($t0)
+	lw $s2, s2($t0)
+	lw $s3, s3($t0)
+	lw $s4, s4($t0)
+	lw $s5, s5($t0)
+	lw $s6, s6($t0)
+	lw $s7, s7($t0)
+	lw $t8, t8($t0)
+	lw $t9, t9($t0)
+	lw $k0, k0($t0)
+	lw $k1, k1($t0)
+	lw $gp, gp($t0)
+	lw $sp, sp($t0)
+	lw $fp, fp($t0)
 	
-	lw $t0, 28($t0) # finally load t0
+	lw $t0, t0($t0) # finally load t0
 	
 	jr $ra
 	
@@ -236,64 +196,67 @@ print_PCB_sequence:
 	addi $sp, $sp, -4
 	sw $t0, 0($sp)
 	
-	new_line
+	li $a0, '\n'
+	print_char
+	
 	la $a0, RUNNING_str
 	print_string
-	space
+	
+	li $a0, ' '
+	print_char
 	
 	lw $s0, RUNNING
 	lw $s1, 140($s0)
 	
 	move $a0, $s0
-	print_pointer
+	print_int
 	
 	la $a0, next_str
 	print_string
 	
 	move $a0, $s1
-	print_pointer
+	print_int
 	
-	new_line
-	##########################
+	li $a0, '\n'
+	print_char
+	
 	la $a0, READY_str
 	print_string
-	space
-	space
-	space
+	
+	li $a0, ' '
+	print_char
+	print_char
+	print_char
 	
 	lw $s0, READY    # task0
 	lw $s1, 140($s0) # task1
 	lw $s2, 140($s1) # task2
 	lw $s3, 140($s2) # null
 	
-	
 	move $a0, $s0
-	print_pointer   # task0
+	print_int   # task0
 	
 	la $a0, next_str
 	print_string
 	
-	#lw $s0, 140($s0) # task1
 	move $a0, $s1
-	print_pointer   # task1
+	print_int   # task1
 	
 	la $a0, next_str
 	print_string
 	
-	#lw $s1, 140($s0) # task2
 	move $a0, $s2
-	print_pointer   # task2
+	print_int   # task2
 	
 	la $a0, next_str
 	print_string
 	
-	#lw $s1, 140($s1) # null
 	move $a0, $s3
-	#li $a0, 0
-	print_pointer   # null
+	print_int   # null
 	
-	new_line
-	new_line
+	li $a0, '\n'
+	print_char
+	print_char
 	
 	lw $t0, 0($sp)
 	addi $sp, $sp, 4
@@ -301,5 +264,5 @@ print_PCB_sequence:
 	
 print_stack_adress:
 	lw $a0, 0($sp)
-	print_pointer
+	print_int
 	jr $ra
